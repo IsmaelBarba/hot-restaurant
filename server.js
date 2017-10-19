a14ae72f2b1f18d241644db96c38ddea7469c4ee
Ismael Barba <ismaelbarba1@gmail.com>
2017-10-18 21:43:15 -0500

    fired up node

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var express =  require("express");
+// Dependencies
+// =============================================================
+var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-var app = express();
 
-var PORT = process.env.PORT || 3200;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 4000;
+
 
 // body parser is for=
 // body parser take the data that we are submitting to the server
@@ -16,22 +21,64 @@ var PORT = process.env.PORT || 3200;
 //"req" is the request that goes to the server. It is always on the request going up to the server
 // "res" is the response that goes from server to the client.
 // "req.body" with be created by body parser
-app.use(bodyParser.urlencoded({
-    extend: false
-}));
 
+//====================================================================
+
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//(DATA)
+// =============================================================
+var allTables = []; // stores the reservations;
 
-
-app.get("/", function(req, res){
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res){
+app.get("/add", function (req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
 });
 
-app.listen(PORT, function(){
-    console.log("App listening on port: " + PORT);
+
+
+// // Search for Specific Character (or all characters) - provides JSON
+// app.get("/api/:characters?", function(req, res) {
+//   var chosen = req.params.characters;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//     for (var i = 0; i < characters.length; i++) {
+//       if (chosen === characters[i].routeName) {
+//         res.json(characters[i]);
+//         return;
+//       }
+//     }
+
+//     res.json(false);
+//   }
+//   else {
+//     res.json(characters);
+//   }
+// });
+
+// // Create New Characters - takes in JSON input
+// app.put("/api/new", function(req, res) {
+//   var newcharacter = req.body;
+
+//   console.log(newcharacter);
+
+//   characters.push(newcharacter);
+
+//   res.json(newcharacter);
+// });
+// Start The Server
+// =============================================================
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
